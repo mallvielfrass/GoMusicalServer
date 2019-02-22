8d831073dc7b1b106eda0d15ec39dbbe0a16a596
mallvielfrass <elliot@127.0.0.1localhost>
2019-02-22 23:38:50 +0300

    add download&convert file

--- main.go
+++ main.go
@@ -3,13 +3,41 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
 	"strings"
 
 	pretiumvkgo "github.com/mallvielfrass/pretiumVKgo"
 )
+
+func downloadFile(filepath string, url string) (err error) {
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Writer the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 type MResult struct {
 	Response MResponse `json:"response"`
@@ -37,59 +65,87 @@ type Musica struct {
 func api(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println(r)
-
 	fmt.Println(r.URL)
 	fmt.Println(r.URL.Host)
 	fmt.Println(r.URL.Path)
 	key := r.URL.Query()
-	mname := string(key["q"][0]) //get music name
-	//res := "audio nome: " + mname
-	//________________________________
-	ready, err := ioutil.ReadFile("key.txt") // Считываем ключ из файла
-	if err != nil {
-		log.Fatalln("Не удалось считать ключ из файла:", err)
+	fmt.Println(key)
+	value, ok := key["q"]
+	if ok {
+		fmt.Println("value: ", value)
+		mname := string(key["q"][0]) //get music name
+		//res := "audio nome: " + mname
+		//________________________________
+		ready, err := ioutil.ReadFile("key.txt") // Считываем ключ из файла
+		if err != nil {
+			log.Fatalln("Не удалось считать ключ из файла:", err)
+		}
+		keys := (strings.Split(string(ready), "\n"))[0]
+		api := pretiumvkgo.NewAPI(keys)
+		fmt.Println("enter music name")
+
+		resVK := api.AudioSearch(mname, 10, 0)
+		//fmt.Println(x)
+		//
+		//fmt.Println(resVK)
+		bx := []byte(resVK)
+		var result MResult
+
+		err = json.Unmarshal(bx, &result)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		R := result.Response.Items
+
+		lenR := len(R)
+
+		//fullMusic := "title:" + R[0].Title + "<br>" + "Artist:" + R[0].Artist + "<br>" + "url: " + R[0].URL
+		var Musa = []Musica{}
+		i := 0
+		for i < lenR {
+			var msg = new(Musica)
+			msg.Title = R[i].Title
+			msg.Artist = R[i].Artist
+			msg.Url = R[i].URL
+			Musa = append(Musa, *msg)
+			i = i + 1
+		}
+		//fmt.Println(fullMusic)
+		//fmt.Println(Musa)
+		jsMusa, err := json.Marshal(Musa)
+		if err != nil {
+			fmt.Printf("Error: %s", err)
+			return
+		}
+		//fmt.Println(string(jsMusa)) //musica
+		//filename := "audio/" + R[0].Title + ".mp3"
+		fmt.Fprintf(w, string(jsMusa))
+	} else {
+		fmt.Println("key q not found")
 	}
-	keys := (strings.Split(string(ready), "\n"))[0]
-	api := pretiumvkgo.NewAPI(keys)
-	fmt.Println("enter music name")
+	value, ok = key["link"]
+	if ok {
 
-	resVK := api.AudioSearch(mname, 10, 0)
-	//fmt.Println(x)
-	//
-	//fmt.Println(resVK)
-	bx := []byte(resVK)
-	var result MResult
-
-	err = json.Unmarshal(bx, &result)
-	if err != nil {
-		log.Fatalln(err)
+		//fmt.Println(value[0])
+		arr := strings.Split(value[0], "cut=")
+		name := arr[1]
+		link := arr[2]
+		fmt.Println(name, "\n", link)
+		nameDown := "music/" + name + ".mp3"
+		nameOpus := "opus/" + name + ".opus"
+		downloadFile(nameDown, link)
+		command := "ffmpeg -i " + "'" + nameDown + "'" + " -c:a libopus -b:a 48k -vbr on -compression_level 10 -frame_duration 60 -application voip " + "'" + nameOpus + "'"
+		fmt.Println(command)
+		out, err := exec.Command("bash", "-c", command).Output()
+		if err != nil {
+			log.Fatal(err)
+			panic("some error found")
+		}
+		fmt.Println(string(out))
+		return (w, nameOpus)
+	} else {
+		fmt.Println("key not found")
 	}
-	R := result.Response.Items
-
-	lenR := len(R)
-
-	//fullMusic := "title:" + R[0].Title + "<br>" + "Artist:" + R[0].Artist + "<br>" + "url: " + R[0].URL
-	var Musa = []Musica{}
-	i := 0
-	for i < lenR {
-		var msg = new(Musica)
-		msg.Title = R[i].Title
-		msg.Artist = R[i].Artist
-		msg.Url = R[i].URL
-		Musa = append(Musa, *msg)
-		i = i + 1
-	}
-	//fmt.Println(fullMusic)
-	//fmt.Println(Musa)
-	jsMusa, err := json.Marshal(Musa)
-	if err != nil {
-		fmt.Printf("Error: %s", err)
-		return
-	}
-	fmt.Println(string(jsMusa))
-	//filename := "audio/" + R[0].Title + ".mp3"
-	fmt.Fprintf(w, string(jsMusa))
-
 }
 func search(w http.ResponseWriter, r *http.Request) {
 
